efdece4a811af98aeef8919df7f0dc8ef29a2de5
Kevin Albarran <recodedkevdev@gmail.com>
2024-05-01 22:28:15 -0700

    updated react, react-dom, and added MUI components

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>test</h1>;
+  return <h1>tsdfssdfsdfdfest</h1>;
 };
 
 export default App;
--- src-react/src/index.jsx
+++ src-react/src/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-const root = document.getElementById('root');
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  root,
-);
+root.render(<App />);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   root,
+// );
